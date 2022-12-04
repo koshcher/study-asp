b30d8b7151c8aec7a889b185402d570afed34775
Roman Koshchei <romankoshchei@gmail.com>
2022-12-04 10:47:49 +0200

    Links and Products

---
+++ Products/Products/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Products.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Products.Controllers
+{
+  public class ProductController : Controller
+  {
+    private List<Product> products;
+
+    // GET: Product
+    public ActionResult Index()
+    {
+      products = new List<Product>()
+      {
+        new Product{ ID = Guid.NewGuid(), CreatedDate= DateTime.Now, Name = "APPLE", Price=10 },
+        new Product{ ID = Guid.NewGuid(), CreatedDate= DateTime.Now, Name = "CHARGER", Price=120 },
+        new Product{ ID = Guid.NewGuid(), CreatedDate= DateTime.Now, Name = "LAPTOP", Price=25000 },
+        new Product{ ID = Guid.NewGuid(), CreatedDate= DateTime.Now, Name = "DISK", Price=520 },
+      };
+
+      return View(products);
+    }
+  }
+}
